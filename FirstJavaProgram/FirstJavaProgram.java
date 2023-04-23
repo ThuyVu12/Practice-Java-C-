4981d1f36fa9de1bd34cf18b0e57f0fcd4963540
Thuy Vu <106577887+ThuyVu12@users.noreply.github.com>
2023-04-23 00:39:16 -0700

    Add files via upload

---
+++ FirstJavaProgram/FirstJavaProgram.java
@@ -0,0 +1,6 @@
+public class FirstJavaProgram {
+
+    public static void main(String[] args) {
+        System.out.printf("My name is Thuy Vu\n" + "I am 27 years old\n" + "My hometown is Bienhoa, Vietnam\n");
+    }
+}
