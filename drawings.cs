9d1068928d285b6e03f79cb97cf730e53215bd38
Thuy Vu <106577887+ThuyVu12@users.noreply.github.com>
2023-05-16 03:02:07 -0700

    Create drawings.cs
    ASCII art

---
+++ drawings.cs
@@ -0,0 +1,35 @@
+using System;
+
+namespace ConsoleCreatures
+{
+  class Program
+  {
+    static void Main()
+    {
+      //to draw some ASCII art
+      Console.WriteLine(" .-.");
+      Console.WriteLine("(o o)");  
+      Console.WriteLine("| O |");
+      Console.WriteLine("|   |");
+      Console.WriteLine("'~~~'");
+      Console.WriteLine("Asui");
+      // another one
+      Console.WriteLine("c_     ___");
+      Console.WriteLine("　 \\\\__|__|__");
+      Console.WriteLine("　　 \\( o_o)");
+      Console.WriteLine("　　　 > ~  >");
+      Console.WriteLine("　　　/ 　 / \\\\");
+      Console.WriteLine("　　 /　　/　 \\\\");
+      Console.WriteLine("　　 \\　 )　　c/");
+      Console.WriteLine("　　/　 /");
+      Console.WriteLine("　 /　/|");
+      Console.WriteLine("　(　( \\");
+      Console.WriteLine("　|　|  \\");
+      Console.WriteLine("　| / \\  )");
+      Console.WriteLine("　| |　) |");
+      Console.WriteLine(" /  )(_/");
+      Console.WriteLine("(_ /");
+      Console.WriteLine("a dancing guy");
+     }
+  }
+}
